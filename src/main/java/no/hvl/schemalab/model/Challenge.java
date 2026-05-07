1403d3e42c9c351dcd076f56b83f60b696772e49
Runar Skjong Røssevold <runarrossevold@gmail.com>
2026-05-07 11:43:02 +0200

    update Challenge.java to include list of challenge variants for diff languages

--- src/main/java/no/hvl/schemalab/model/Challenge.java
+++ src/main/java/no/hvl/schemalab/model/Challenge.java
@@ -1,10 +1,13 @@
 package no.hvl.schemalab.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
 public class Challenge {
 
     @Id
@@ -32,75 +35,6 @@ public class Challenge {
     @OneToMany(mappedBy = "challenge", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<TestCase> testCases = new ArrayList<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getStarterCode() {
-        return starterCode;
-    }
-
-    public void setStarterCode(String starterCode) {
-        this.starterCode = starterCode;
-    }
-
-    public String getHarnessCode() {
-        return harnessCode;
-    }
-
-    public void setHarnessCode(String harnessCode) {
-        this.harnessCode = harnessCode;
-    }
-
-    public List<SchemaDoc> getSchemas() {
-        return schemas;
-    }
-
-    public void setSchemas(List<SchemaDoc> schemas) {
-        this.schemas = schemas;
-    }
-
-    public List<TestCase> getTestCases() {
-        return testCases;
-    }
-
-    public void setTestCases(List<TestCase> testCases) {
-        this.testCases = testCases;
-    }
+    @OneToMany(mappedBy = "challenge", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<ChallengeVariant> variants = new ArrayList<>();
 }
